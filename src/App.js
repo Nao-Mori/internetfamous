b0e6a4b0ea2ed5f9d2c1cd6be59016950cb889d6
nao mori <lockn980@gmail.com>
2020-04-09 13:08:29 +0200

    Update read me

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 0 20px;
+  text-align: center;
+`
 
 function App() {
   return (
-    <div>
-    </div>
+    <Wrapper>
+      <h1>Hello World!</h1>
+    </Wrapper> 
   );
 }
 
